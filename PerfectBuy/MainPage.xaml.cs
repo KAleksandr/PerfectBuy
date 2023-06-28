aa33c2159ce4231bb78e35e38e1e6de013db8878
KAleksandr <okuzmenkoa@gmail.com>
2023-06-28 22:23:35 +0300

    Add logic the slider

--- PerfectBuy/MainPage.xaml.cs
+++ PerfectBuy/MainPage.xaml.cs
@@ -2,13 +2,31 @@
 
 public partial class MainPage : ContentPage
 {
-	
+	decimal bill;
+	int tip;
+	int noPersons = 1;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	
+    private void txtBill_Completed(object sender, EventArgs e)
+    {
+		bill = decimal.Parse(txtBill.Text);
+		CalculateTotal();
+    }
+
+    private void CalculateTotal()
+    {
+        
+    }
+
+    private void sldTip_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        tip = (int)sldTip.Value;
+        lblTip.Text = $"Tip: {tip}%";
+        CalculateTotal();
+    }
 }
 
