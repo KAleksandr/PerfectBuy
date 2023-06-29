2cbeaa09822a1f329feebffdeebc54f2f2776933
KAleksandr <okuzmenkoa@gmail.com>
2023-06-29 12:54:59 +0300

    Added logic calculate

--- PerfectBuy/MainPage.xaml.cs
+++ PerfectBuy/MainPage.xaml.cs
@@ -19,14 +19,56 @@ public partial class MainPage : ContentPage
 
     private void CalculateTotal()
     {
-        
+        //Total tip
+        var totalTip = (bill * tip) / 100;
+        //Tip by person
+        var tipByPerson = (totalTip / noPersons);
+        lblTipByPerson.Text = $"{tipByPerson:C}";
+        //SubTotall
+        var subTotal = (bill / noPersons);
+        lblSubtotal.Text = $"{subTotal:C}";
+        //Total
+        var totalByPerson = (bill + totalTip) / noPersons;
+        lblTotal.Text = $"{totalByPerson:C}";
+
     }
 
     private void sldTip_ValueChanged(object sender, ValueChangedEventArgs e)
     {
         tip = (int)sldTip.Value;
         lblTip.Text = $"Tip: {tip}%";
-        CalculateTotal();
+        CalculateTotal(); 
+    }
+
+    private void Btn_Clicked(object sender, EventArgs e)
+    {
+        if (sender is Button)
+        {
+            var btn = (Button)sender;
+            var percentage = int.Parse(btn.Text.Replace("%", ""));
+            sldTip.Value = percentage;
+            
+        }
+    }
+
+    private void BtnPlusMinus_Clicked(object sender, EventArgs e)
+    {
+        if (sender is Button)
+        {
+            var btn = (Button)sender;
+            
+            if (btn.Text.Equals("-") && noPersons >1)
+            {
+                noPersons--;
+                
+            }
+            else if (btn.Text.Equals("+"))
+            {
+                noPersons++;
+            }
+            lblNoPersons.Text = noPersons.ToString();
+            CalculateTotal();
+        }
     }
 }
 
